3b8a89b9e74b55df796831a53168707373d23bcf
Himanshu56-56 <himanshugohar907@gmail.com>
2026-04-27 16:44:38 +0530

    fixed port issue

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use("/api/data", serviceRoute);
 app.use("/api/admin",adminRoute);
 app.use(errorMiddleware);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 connectDb().then(()=>{
 app.listen(PORT,()=>{
     console.log(`server is running at port:${PORT}`);
